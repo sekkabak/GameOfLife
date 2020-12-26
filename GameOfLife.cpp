ef6888ebfa27c0e51bd44b2483727e28a47c814a
Cezary Bąk <sekkabak@gmail.com>
2020-12-26 17:35:59 +0100

    final

--- GameOfLife.cpp
+++ GameOfLife.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 #include <thread>
 
-using namespace std;
-
+// left arrow start/stop simulation
+// top/down arrow speed/slow simulation speed
+// right arrow single step in simulation
 class GameOfLife
 {
 	HANDLE consoleOutputHandle;
@@ -12,93 +13,96 @@ class GameOfLife
 	SMALL_RECT rectWindow;
 	int screenWidth = 80;
 	int screenHeight = 80;
-	bool new_map[80][80];
-	bool map[80][80];
-	int num_of_cells = 80;
+	bool new_map_[80][80];
+	bool map_[80][80];
 
-	short m_keyOldState[256] = { 0 };
-	short m_keyNewState[256] = { 0 };
+	double simulation_speed = 1.0;
+	double simulation_speed_step = .05;
+	bool simulation_playing = false;
 
-	double symulation_speed = 1.0;
-	double symulation_speed_step = .1;
-	bool symulation_playing = false;
-
-	struct sKeyState
+	void SetCell(const bool state, const int x, const int y)
 	{
-		bool bPressed;
-		bool bReleased;
-		bool bHeld;
-	} m_keys[256];
-
-	void SetCell(bool state, int x, int y)
-	{
-		map[x][y] = state;
-		Draw(x, y, ' ', 0xff);
+		map_[x][y] = state;
 	}
 
-	int GetLivingNeighbourCellsCount(int x, int y)
+	int GetLivingNeighbourCellsCount(const int x, const int y)
 	{
 		int sum = 0;
 
-		// górna linia
-		if (y > 0 && x > 0 && map[x - 1][y - 1]) sum++;
-		if (y > 0 && map[x][y - 1]) sum++;
-		if (y > 0 && x < screenWidth - 1 && map[x + 1][y - 1]) sum++;
+		// top line
+		if (y > 0 && x > 0 && map_[x - 1][y - 1]) sum++;
+		if (y > 0 && map_[x][y - 1]) sum++;
+		if (y > 0 && x < screenWidth - 1 && map_[x + 1][y - 1]) sum++;
 
-		// srodkowa linia
-		if (x > 0 && map[x - 1][y]) sum++;
-		if (x < screenWidth - 1 && map[x + 1][y]) sum++;
+		// middle line
+		if (x > 0 && map_[x - 1][y]) sum++;
+		if (x < screenWidth - 1 && map_[x + 1][y]) sum++;
 
-		// dolna linia
-		if (y < screenHeight - 1 && x > 0 && map[x - 1][y + 1]) sum++;
-		if (y < screenHeight - 1 && map[x][y + 1]) sum++;
-		if (y < screenHeight - 1 && x < screenWidth - 1 && map[x + 1][y + 1]) sum++;
+		// bottom line
+		if (y < screenHeight - 1 && x > 0 && map_[x - 1][y + 1]) sum++;
+		if (y < screenHeight - 1 && map_[x][y + 1]) sum++;
+		if (y < screenHeight - 1 && x < screenWidth - 1 && map_[x + 1][y + 1]) sum++;
 
 		return sum;
 	}
 
-	void MakeStep()
+	void ClearNewMap()
 	{
-		for (int x = 0; x < num_of_cells; x++) {
-			for (int y = 0; y < num_of_cells; y++) {
-				new_map[x][y] = false;
-			}
-		}
-		
-		for (int x = 0; x < num_of_cells; x++) {
-			for (int y = 0; y < num_of_cells; y++) {
-				const int livingNeighbours = GetLivingNeighbourCellsCount(x, y);
+		for (int x = 0; x < screenWidth; x++)
+			for (int y = 0; y < screenHeight; y++)
+				new_map_[x][y] = false;
+	}
 
-				if (map[x][y] == true) {
-					// overpopulation
-					if (livingNeighbours >= 4) {
-						new_map[x][y] = false;
-					}
-					// survives
-					else if (livingNeighbours >= 2) {
-						// rewrite
-						new_map[x][y] = map[x][y];
-					}
-					// solitude
-					else if (livingNeighbours <= 1) {
-						new_map[x][y] = false;
-					}
-				} else {
-					// birth
-					if (livingNeighbours == 3) {
-						new_map[x][y] = true;
-					}
-				}
-			}
-		}
+	void SaveNewMapToActualMap()
+	{
+		for (int x = 0; x < screenWidth; x++)
+			for (int y = 0; y < screenHeight; y++)
+				map_[x][y] = new_map_[x][y];
+	}
 
-		for (int x = 0; x < num_of_cells; x++) {
-			for (int y = 0; y < num_of_cells; y++) {
-				map[x][y] = new_map[x][y];
+	// Calculates if given cell should die or spawn
+	void HandleCellInUnitOfTime(const int x, const int y)
+	{
+		const int living_neighbours = GetLivingNeighbourCellsCount(x, y);
+
+		if (map_[x][y] == true)
+		{
+			// dies bcs of overpopulation
+			if (living_neighbours >= 4)
+				new_map_[x][y] = false;
+				// survives
+			else if (living_neighbours >= 2)
+				new_map_[x][y] = map_[x][y]; // rewrite
+				// dies bcs of solitude
+			else if (living_neighbours <= 1)
+				new_map_[x][y] = false;
+		}
+		else
+		{
+			// birth of new cell
+			if (living_neighbours == 3)
+			{
+				new_map_[x][y] = true;
 			}
 		}
 	}
 
+	// Step in simulation means that one unit of time for all cells has passed
+	void MakeStep()
+	{
+		ClearNewMap();
+
+		// Handle all cell changes in one unit of time
+		for (int x = 0; x < screenWidth; x++)
+			for (int y = 0; y < screenHeight; y++)
+				HandleCellInUnitOfTime(x, y);
+
+		SaveNewMapToActualMap();
+	}
+
+	// Lot of things to set console for proper displaing
+	// Most of things are from olcConsoleGameEngine by javidx9
+	// Check him out https://www.youtube.com/c/javidx9 :)
 	void InitConsole()
 	{
 		consoleOutputHandle = GetStdHandle(STD_OUTPUT_HANDLE);
@@ -106,7 +110,6 @@ class GameOfLife
 
 		SMALL_RECT rect = {0, 0, 1, 1};
 		SetConsoleWindowInfo(consoleOutputHandle, TRUE, &rect);
-
 
 		COORD coord = {static_cast<short>(screenWidth), static_cast<short>(screenHeight)};
 		SetConsoleScreenBufferSize(consoleOutputHandle, coord);
@@ -125,15 +128,16 @@ class GameOfLife
 
 		wcscpy_s(cfi.FaceName, L"Consolas");
 		SetCurrentConsoleFontEx(consoleOutputHandle, false, &cfi);
-		rectWindow = {0, 0, static_cast<short>(screenWidth) - 1, static_cast<short>(screenHeight) - 1};
+		rectWindow = {0, 0, static_cast<short>(screenWidth - 1), static_cast<short>(screenHeight - 1)};
 		SetConsoleWindowInfo(consoleOutputHandle, TRUE, &rectWindow);
 		SetConsoleMode(consoleInputHandle, ENABLE_EXTENDED_FLAGS | ENABLE_WINDOW_INPUT | ENABLE_MOUSE_INPUT);
 	}
 
 	void Draw(const int x, const int y, const short c, const short color = 0x000F)
 	{
-		if (x >= 0 && y >= 0 && x < screenWidth && y < screenHeight) {
-			int index = y * screenWidth + x;
+		if (x >= 0 && y >= 0 && x < screenWidth && y < screenHeight)
+		{
+			const int index = y * screenWidth + x;
 			screenBuffer[index].Char.UnicodeChar = c;
 			screenBuffer[index].Attributes = color;
 		}
@@ -141,181 +145,212 @@ class GameOfLife
 
 	void ReDrawMap()
 	{
-		for (int y = 0; y < num_of_cells; y++) {
-			for (int x = 0; x < num_of_cells; x++) {
-				if (map[x][y])
-					Draw(x, y, ' ', 0xff);
+		for (int x = 0; x < screenWidth; x++)
+		{
+			for (int y = 0; y < screenHeight; y++)
+			{
+				if (map_[x][y])
+					Draw(x, y, ' ', 0xff); // this makes all pixels in char space to be white(dunno why but its cool) 
 				else
 					Draw(x, y, ' ');
 			}
 		}
 	}
 
+	short actual_key_state_[4];
+	short old_key_state_[4];
+
+	struct KeyState
+	{
+		bool pressed;
+		bool released;
+	} arrow_keys_[4];
+
+#define ARROW_LEFT_INDEX 0
+#define ARROW_UP_INDEX 1
+#define ARROW_RIGHT_INDEX 2
+#define ARROW_DOWN_INDEX 3
+
+	// Reads key state which is saved in actual_key_state_
+	// Also compares new key state to old key state for key release
+	// Reads only arrow keys
 	void HandleInput()
 	{
-		// Handle Keyboard Input
-		for (int i = 0; i < 256; i++) {
-			m_keyNewState[i] = GetAsyncKeyState(i);
+		// Keycodes:
+		// 37 - arrow left
+		// 38 - arrow up
+		// 39 - arrow right
+		// 40 - arrow down
 
-			m_keys[i].bPressed = false;
-			m_keys[i].bReleased = false;
+		int key_map[4] = {37, 38, 39, 40};
+		for (int i = 0; i < 4; i++)
+		{
+			// My simulation is running on other thread so I use async
+			actual_key_state_[i] = GetAsyncKeyState(key_map[i]);
+			arrow_keys_[i].pressed = false;
+			arrow_keys_[i].released = false;
 
-			if (m_keyNewState[i] != m_keyOldState[i])
+			// Avoid multiple onPress
+			if (actual_key_state_[i] != old_key_state_[i])
 			{
-				if (m_keyNewState[i] & 0x8000)
+				// GetAsyncKeyState returns 2 bytes number
+				// If the most significant bit is set, the key is down
+				if (actual_key_state_[i] >> 15 == 0x1)
 				{
-					m_keys[i].bPressed = !m_keys[i].bHeld;
-					m_keys[i].bHeld = true;
+					arrow_keys_[i].pressed = true;
 				}
 				else
 				{
-					m_keys[i].bReleased = true;
-					m_keys[i].bHeld = false;
+					arrow_keys_[i].released = true;
 				}
 			}
 
-			m_keyOldState[i] = m_keyNewState[i];
+			old_key_state_[i] = actual_key_state_[i];
 		}
+	}
+
+	void UpdateConsoleTitle()
+	{
+		wchar_t s[256];
+		swprintf_s(s, 256, L"Game Of Life - Simulation Speed: %3.2f", 1.0 - simulation_speed);
+		SetConsoleTitle(s);
+	}
+
+	void ModifySimulationSpeed(double mod_number)
+	{
+		simulation_speed += mod_number;
+		if (this->simulation_speed <= 0)
+			this->simulation_speed = 0;
+
+		UpdateConsoleTitle();
 	}
 
 	void GameThread()
 	{
+		UpdateConsoleTitle();
 		OnStart();
 		GameLoop();
 	}
 
 	double sim_time = 0.0;
+
 	void GameLoop()
 	{
 		while (true)
 		{
 			HandleInput();
-			if (this->symulation_playing) {
-				if (sim_time >= this->symulation_speed) {
+			if (this->simulation_playing)
+			{
+				if (sim_time >= this->simulation_speed)
+				{
 					MakeStep();
 					sim_time = 0.0;
-				} else {
-					Sleep(10 * symulation_speed_step);
-					sim_time += symulation_speed_step;
+				}
+				else
+				{
+					Sleep(10 * simulation_speed_step);
+					sim_time += simulation_speed_step;
 				}
 			}
-		
+
 			OnUpdate();
 			WriteConsoleOutput(consoleOutputHandle, screenBuffer,
-				{
-					static_cast<short>(screenWidth),
-					static_cast<short>(screenHeight)
-				},
-				{ 0, 0 },
-				&rectWindow);
+			                   {
+				                   static_cast<short>(screenWidth),
+				                   static_cast<short>(screenHeight)
+			                   },
+			                   {0, 0},
+			                   &rectWindow);
 		}
-	}
-
-	void OnStart()
-	{
-		// square
-		// SetCell(true, 10, 70);
-		// SetCell(true, 11, 70);
-		// SetCell(true, 12, 70);	
-		// SetCell(true, 10, 71);
-		// SetCell(true, 12, 71);
-		// SetCell(true, 10, 72);
-		// SetCell(true, 11, 72);
-		// SetCell(true, 12, 72);
-
-		// flying sth
-		// SetCell(true, 6, 3);
-		// SetCell(true, 7, 4);
-		// SetCell(true, 5, 5);
-		// SetCell(true, 6, 5);
-		// SetCell(true, 7, 5);
-
-		// shooting sth
-		SetCell(true, 34, 6);
-		SetCell(true, 34, 7);
-		SetCell(true, 35, 6);
-		SetCell(true, 35, 7);
-
-		SetCell(true, 39, 6);
-		SetCell(true, 40, 5);
-		SetCell(true, 41, 4);
-		SetCell(true, 40, 7);
-		SetCell(true, 41, 8);
-		SetCell(true, 42, 5);
-		SetCell(true, 42, 6);
-		SetCell(true, 42, 7);
-		SetCell(true, 42, 5);
-		SetCell(true, 43, 3);
-		SetCell(true, 43, 4);
-		SetCell(true, 43, 8);
-		SetCell(true, 43, 9);
-
-		SetCell(true, 57, 8);
-		SetCell(true, 58, 7);
-		SetCell(true, 58, 8);
-		SetCell(true, 58, 9);
-		SetCell(true, 59, 6);
-		SetCell(true, 59, 7);
-		SetCell(true, 59, 8);
-		SetCell(true, 59, 9);
-		SetCell(true, 59, 10);
-		SetCell(true, 60, 5);
-		SetCell(true, 60, 6);
-		SetCell(true, 60, 10);
-		SetCell(true, 60, 11);
-
-		SetCell(true, 64, 7);
-		SetCell(true, 64, 8);
-		SetCell(true, 64, 9);
-		SetCell(true, 65, 7);
-		SetCell(true, 65, 8);
-		SetCell(true, 65, 9);
-
-		SetCell(true, 68, 8);
-		SetCell(true, 68, 9);
-		SetCell(true, 69, 8);
-		SetCell(true, 69, 9);
 	}
 
 	void OnUpdate()
 	{
-		// arrow up
-		if (m_keys[38].bReleased)
-		{
-			// symulation speed up
-			symulation_speed -= symulation_speed_step;
-			if (this->symulation_speed <= 0)
-				this->symulation_speed = 0;
-		}
+		// simulation on/off
+		if (arrow_keys_[ARROW_LEFT_INDEX].released) simulation_playing = !simulation_playing;
 
-		// arrow down
-		if (m_keys[40].bReleased)
-		{
-			// symulation speed down
-			symulation_speed += symulation_speed_step;
-		}
-		
-		// arrow right
-		if (m_keys[39].bReleased)
-		{
-			// dalszy krok symulacji
-			MakeStep();
-		}
+		// simulation speed up
+		if (arrow_keys_[ARROW_UP_INDEX].released) ModifySimulationSpeed(-simulation_speed_step);
 
-		// arrow left
-		if (m_keys[37].bReleased)
-		{
-			symulation_speed = 1.0;
-			symulation_playing = !symulation_playing;
-		}
+		// simulation speed down
+		if (arrow_keys_[ARROW_DOWN_INDEX].released) ModifySimulationSpeed(simulation_speed_step);
+
+		// simulation step forward
+		if (arrow_keys_[ARROW_RIGHT_INDEX].released) MakeStep();
 
 		ReDrawMap();
 	}
 
+protected:
+
+	// Here I can fill map with living cells for simulation to actuall work
+	void OnStart()
+	{
+		// shooting sth
+		DrawShootingSth(0, 0);
+		DrawShootingSth(20, 20);
+		DrawShootingSth(40, 40);
+	}
+
 public:
+
+	// Draws shooting entity on map
+	// Starts on given x and y
+	void DrawShootingSth(const int x, const int y)
+	{
+		SetCell(true, x + 4, y + 6);
+		SetCell(true, x + 4, y + 7);
+		SetCell(true, x + 5, y + 6);
+		SetCell(true, x + 5, y + 7);
+
+		SetCell(true, x + 9, y + 6);
+		SetCell(true, x + 10, y + 5);
+		SetCell(true, x + 11, y + 4);
+		SetCell(true, x + 10, y + 7);
+		SetCell(true, x + 11, y + 8);
+		SetCell(true, x + 12, y + 5);
+		SetCell(true, x + 12, y + 6);
+		SetCell(true, x + 12, y + 7);
+		SetCell(true, x + 12, y + 5);
+		SetCell(true, x + 13, y + 3);
+		SetCell(true, x + 13, y + 4);
+		SetCell(true, x + 13, y + 8);
+		SetCell(true, x + 13, y + 9);
+
+		SetCell(true, x + 27, y + 8);
+		SetCell(true, x + 28, y + 7);
+		SetCell(true, x + 28, y + 8);
+		SetCell(true, x + 28, y + 9);
+		SetCell(true, x + 29, y + 6);
+		SetCell(true, x + 29, y + 7);
+		SetCell(true, x + 29, y + 8);
+		SetCell(true, x + 29, y + 9);
+		SetCell(true, x + 29, y + 10);
+		SetCell(true, x + 30, y + 5);
+		SetCell(true, x + 30, y + 6);
+		SetCell(true, x + 30, y + 10);
+		SetCell(true, x + 30, y + 11);
+
+		SetCell(true, x + 34, y + 7);
+		SetCell(true, x + 34, y + 8);
+		SetCell(true, x + 34, y + 9);
+		SetCell(true, x + 35, y + 7);
+		SetCell(true, x + 35, y + 8);
+		SetCell(true, x + 35, y + 9);
+
+		SetCell(true, x + 38, y + 8);
+		SetCell(true, x + 38, y + 9);
+		SetCell(true, x + 39, y + 8);
+		SetCell(true, x + 39, y + 9);
+	}
+
 	GameOfLife()
 	{
 		InitConsole();
+	}
+
+	void InitGame()
+	{
 		auto t = std::thread(&GameOfLife::GameThread, this);
 		t.join();
 	}
@@ -324,6 +359,7 @@ public:
 int main()
 {
 	GameOfLife gof;
-
+	gof.InitGame();
+	
 	return 0;
 }
